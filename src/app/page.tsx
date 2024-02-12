e09b772012f4cd0527a46922bf83fb2700be109c
Joshua Thornton <JoshuaThornton.SoftwareDev@gmail.com>
2024-02-12 17:35:24 -0500

    Added dependency for later use - heroicons
    Also moved contribution comment to more appropriate location

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image';
 import Page_With_BG from './components/page_with_bg/page_with_bg';
 
-// Image by evening_tao on Freepik : https://www.freepik.com/free-photo/restaurant-interior_1243339.htm#query=restaurant%20interior&position=1&from_view=keyword&track=ais&uuid=67580ef5-36d1-4b96-92d2-47a2336d3629
 export default function HomePage() {
   return (
     <Page_With_BG
+      // Image by evening_tao on Freepik : https://www.freepik.com/free-photo/restaurant-interior_1243339.htm#query=restaurant%20interior&position=1&from_view=keyword&track=ais&uuid=67580ef5-36d1-4b96-92d2-47a2336d3629
       src='/restaurant-interior.jpg'
       alt='Home page background - restaurant interior'
     >
